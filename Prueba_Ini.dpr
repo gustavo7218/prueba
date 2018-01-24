3a9a63fa26add0e6b1ef98a5ba12800fd1fc871b
gustavo7218 <gustavo7218@outlook.com>
2018-01-24 16:17:22 -0500

    Inicio

---
+++ Prueba_Ini.dpr
@@ -0,0 +1,14 @@
+program Prueba_Ini;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
